9a22af46451730d9a3c9423a13fa01ebe1e76471
Jimena Hernández <her21199@uvg.edu.gt>
2025-05-06 06:35:54 -0600

    update mision and vision

--- app/[locale]/misionandvision/page.jsx
+++ app/[locale]/misionandvision/page.jsx
@@ -16,20 +16,20 @@ export default async function MisionVision({ params: { locale } }) {
       <div className="page bg-secondary min-h-screen text-primary">
         <HeaderItem v={"v3"} nav={'/'} />
 
-        <section className="max-w-4xl mx-auto space-y-12 mt-12 px-10">
+        <section className="mx-auto space-y-12 px-10">
           <div>
             <h2 className="mision_vision_title">{t('misionvision:mision')}</h2>
-            <p className="text-lg leading-7">{t('misionvision:mision_desc')}</p>
+            <p className="mision_vision_text">{t('misionvision:mision_desc')}</p>
           </div>
 
           <div>
             <h2 className="mision_vision_title ">{t('misionvision:vision')}</h2>
-            <p className="text-lg leading-7">{t('misionvision:vision_desc')}</p>
+            <p className="mision_vision_text">{t('misionvision:vision_desc')}</p>
           </div>
 
           <div>
             <h2 className="mision_vision_title">{t('misionvision:values')}</h2>
-            <ul className="list-disc list-inside text-lg space-y-1">
+            <ul className="list-disc list-inside mision_vision_text">
               {t('misionvision:values_desc')
                 .split('<br>')
                 .map((value, index) => (
